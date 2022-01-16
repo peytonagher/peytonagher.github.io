ba331aab85b26cb7e9bc358ec592c6bc12080054
peytonagher <pnagher@gmail.com>
2022-01-16 17:37:00 -0500

    11622

---
+++ galaga (test)/rungame.py
@@ -0,0 +1,9 @@
+import pygame
+
+def run(self):
+    while not self.done:
+        dt = self.clock.tick(self.fps)
+        self.event_loop
+        self.update(dt)
+        self.draw
+        pygame.display.update()
